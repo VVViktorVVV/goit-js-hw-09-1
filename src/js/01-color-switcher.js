d074eadd75007a8716cd821d719e6dca339f6db6
Viktor Bondar <vktrbndr@gmail.com>
2021-11-29 23:43:57 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const body = document.querySelector('.body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let interval = null;
+
+function changeInterval() {
+    interval = setInterval(changeBgColor, 1000);
+}
+
+function changeBgColor() {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = `${color}`;
+   
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnStart.addEventListener('click', changeInterval);
